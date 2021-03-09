113f8de12d4e775d9a37ee925ab6a04c742468ed
drjofu <drjofu@web.de>
2021-03-09 13:27:19 +0100

    Tag 1

---
+++ MVVMMini/MVVMMini/ActionCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMMini
+{
+  public class ActionCommand : ICommand
+  {
+    public string DisplayText { get; set; }
+    public string ToolTipText { get; set; }
+
+    public ActionCommand(Action action)
+    {
+      this.action = action;
+    }
+
+    public ActionCommand(Action<object> action2)
+    {
+      this.action2 = action2;
+    }
+
+    private bool isEnabled = true;
+    private readonly Action action;
+    private readonly Action<object> action2;
+
+    public bool IsEnabled
+    {
+      get { return isEnabled; }
+      set
+      {
+        isEnabled = value;
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+      }
+    }
+
+    #region ICommand
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+      return isEnabled;
+    }
+
+    public void Execute(object parameter)
+    {
+      action?.Invoke();
+      action2?.Invoke(parameter);
+    }
+    #endregion
+  }
+}
---
+++ MVVMMini/MVVMMini/ViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMMini
+{
+  public class ViewModel : INotifyPropertyChanged
+  {
+    private int zahl1;
+
+    public int Zahl1
+    {
+      get { return zahl1; }
+      set { zahl1 = value; OnPropertyChanged(); }
+    }
+
+    private int zahl2;
+
+    public int Zahl2
+    {
+      get { return zahl2; }
+      set { zahl2 = value;  OnPropertyChanged(); }
+    }
+
+    private int ergebnis;
+
+    public int Ergebnis
+    {
+      get { return ergebnis; }
+      set
+      {
+        if (ergebnis == value) return;
+        ergebnis = value;
+        //OnPropertyChanged(nameof(Ergebnis));
+        OnPropertyChanged();
+      }
+    }
+
+    public ActionCommand PlusCommand { get; set; }
+    public ActionCommand MinusCommand { get; set; }
+
+    public List<ActionCommand> Commands { get; set; }
+
+
+    public ViewModel()
+    {
+      PlusCommand = new ActionCommand(Plus) { DisplayText = "+", ToolTipText = "Addition" };
+      MinusCommand = new ActionCommand(Minus) { DisplayText = "-", ToolTipText = "Subtraktion" };
+
+      Commands = new List<ActionCommand>
+      {
+        PlusCommand,
+        MinusCommand,
+        new ActionCommand(()=>Ergebnis=Zahl1 * Zahl2){ DisplayText="*", ToolTipText="Plutimikation"}
+      };
+
+    }
+
+    private void Plus()
+    {
+      // hier würde auf das Model zugegriffen werden
+      Ergebnis = Zahl1 + Zahl2;
+      PlusCommand.IsEnabled = false;
+    }
+
+    private void Minus()
+    {
+      Ergebnis = Zahl1 - Zahl2;
+      PlusCommand.IsEnabled = true;
+    }
+
+
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+  }
+}
